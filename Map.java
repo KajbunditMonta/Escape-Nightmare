5b342c5f1a44e4a4ecc85fc8d091de0a30279352
Kajbundit <gadbandidmonta@gmail.com>
2025-09-28 11:06:59 +0700

    add map 2

--- Map.java
+++ Map.java
@@ -2,6 +2,7 @@
 // 2 = monster
 // 3 = stone
 // 4 = trap
+// 5 = trap 2 phase
 // 9 = door
 
 public class Map {
@@ -14,13 +15,28 @@ public class Map {
             {1, 0, 0, 1, 1, 1, 1, 1}, 
             {1, 0, 3, 0, 0, 3, 0, 1}, 
             {1, 0, 3, 0, 3, 0, 0, 9} 
+        },
+        {   
+            {1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 1, 1}, 
+            {1, 2, 1, 4, 4, 0, 0}, 
+            {0, 4, 1, 1, 3, 3, 3}, 
+            {0, 0, 1, 1, 0, 4, 0}, 
+            {0, 0, 1, 1, 0, 2, 0}, 
+            {1, 1, 1, 1, 9, 0, 2}
         }
     };
 
     public Map() {}
 
     public int[][] getMap(int level) {
-        return map[level];
+        int[][] copy = new int[map[level].length][map[level][0].length];
+        for (int i = 0; i < map[level].length; i++) {
+            for (int j = 0; j < map[level][i].length; j++) {
+                copy[i][j] = map[level][i][j];
+            } 
+        }
+        return copy;
     }
 
 }
